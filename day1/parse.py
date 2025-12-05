9544c31bc392f413c11a6833875ec72c5ead5a92
Jay Timbol <jlc.timbol@gmail.com>
2025-12-04 16:20:46 -0800

    day 1 part 2

--- day1/parse.py
+++ day1/parse.py
@@ -8,6 +8,7 @@ with open('day1/input.txt', 'r') as file:
         turnAmt = int(currLine[1:]) #rest of string = num 
         if turnAmt > 100: #handle turns that revolve more than once
             numCycles = turnAmt // 100
+            zeroes += numCycles
             for i in range(numCycles):
                 turnAmt -= 100
         if direction == 'L' and position >= turnAmt: #turn left and doesnt go past 0
@@ -15,6 +16,8 @@ with open('day1/input.txt', 'r') as file:
                 zeroes += 1
             position -= turnAmt
         elif direction == 'L' and position < turnAmt: #turn left and does go past 0
+            if position != 0:
+                zeroes += 1
             position = 100 - (turnAmt - position)
         elif direction == 'R' and position + turnAmt <= 100: #turn right and doesnt go past 0
             if position + turnAmt == 100: #turns to 0
@@ -24,7 +27,8 @@ with open('day1/input.txt', 'r') as file:
                 position += turnAmt
         elif direction == 'R' and position + turnAmt > 100: #turn right and does go past 0
             position = (position + turnAmt) - 100
-            
+            zeroes += 1
+
 print(zeroes)
 
     
